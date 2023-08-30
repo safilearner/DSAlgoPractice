ef800efaeb42eb421735f0f3548929c84d135389
Mohamed Safi <safiizz24@gmail.com>
2023-08-30 22:37:51 +0530

    Day 5: Sum of all the elements in an array, sum of all the elements in an array list

---
+++ src/com/scaler/arrays/SumOfAllTheElements.java
@@ -0,0 +1,41 @@
+package com.scaler.arrays;
+
+import java.util.ArrayList;
+
+public class SumOfAllTheElements {
+
+    public static void main(String[] args) {
+        SumOfAllTheElements totalSum = new SumOfAllTheElements();
+        int[] inputArr = {1, 2, 3, 4, 5};
+        int result = totalSum.sum(inputArr);
+        System.out.println(result);
+
+        ArrayList<Integer> inputList = new ArrayList<>();
+        inputList.add(1);
+        inputList.add(2);
+        inputList.add(3);
+        inputList.add(4);
+        inputList.add(5);
+
+        int resultI = totalSum.sumAList(inputList);
+        System.out.println(resultI);
+    }
+
+    public int sum(int[] A) {
+        int total = 0;
+        int N = A.length;
+        for (int i = 0; i < N; i++) {
+            total += A[i];
+        }
+        return total;
+    }
+
+    public int sumAList(ArrayList<Integer> aList) {
+        int total = 0;
+        int N = aList.size();
+        for (int i = 0; i < N; i++) {
+            total += aList.get(i);
+        }
+        return total;
+    }
+}
